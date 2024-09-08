efd894bf076adc2c57a8b257300a6856da2af1ed
khanhdev99 <khanhnguyennhuk99@gmail.com>
2024-09-08 22:42:19 +0700

    ver 1.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_tutorial/constants.dart';
+import 'package:tiktok_tutorial/views/screens/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget{
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const Text('Hello World'),
+      home:  LoginScreen(),
     );
   }
 }
